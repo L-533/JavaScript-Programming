23306108d790f3fce5bf170bc93cfd4ea72c60a1
LuisLinares <98188267+L-533@users.noreply.github.com>
2025-06-15 19:08:06 -0500

    Update Two_Sum.js

--- Two_Sum.js
+++ Two_Sum.js
@@ -21,3 +21,18 @@ var twoSum = function(nums, target) {
 
    return result || []
 };
+
+
+//Solution 2
+
+var twoSum = function(nums, target) {
+    const pairIdx = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        if (target - num in pairIdx) {
+            return [i, pairIdx[target - num]];
+        }
+        pairIdx[num] = i;
+    }    
+};
