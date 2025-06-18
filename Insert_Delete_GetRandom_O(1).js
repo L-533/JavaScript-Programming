aac7e8e6a5f9a2c2a50c3e53cfb123876020fcd5
LuisLinares <98188267+L-533@users.noreply.github.com>
2025-06-17 19:51:55 -0500

    Update Insert_Delete_GetRandom_O(1).js

--- Insert_Delete_GetRandom_O(1).js
+++ Insert_Delete_GetRandom_O(1).js
@@ -53,3 +53,40 @@ RandomizedSet.prototype.getRandom = function() {
  * var param_2 = obj.remove(val)
  * var param_3 = obj.getRandom()
  */
+//solution 2
+
+class RandomizedSet {
+    constructor() {
+        this.list = [];
+        this.map = new Map();
+    }
+
+    search(val) {
+        return this.map.has(val);
+    }
+
+    insert(val) {
+        if (this.search(val)) return false;
+
+        this.list.push(val);
+        this.map.set(val, this.list.length - 1);
+        return true;
+    }
+
+    remove(val) {
+        if (!this.search(val)) return false;
+
+        const index = this.map.get(val);
+        this.list[index] = this.list[this.list.length - 1];
+        this.map.set(this.list[index], index);
+        this.list.pop();
+        this.map.delete(val);
+        return true;
+    }
+
+    getRandom() {
+        const randomIndex = Math.floor(Math.random() * this.list.length);
+        return this.list[randomIndex];
+    }
+}
+
